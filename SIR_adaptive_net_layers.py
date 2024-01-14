1df4b58ca963472ac1c19a8c49ea1df573de9115
jimmycalvo <jimjocamon94@gmail.com>
2024-01-14 10:22:22 -0600

    Fixed Sporadic Experiment

--- SIR_adaptive_net_layers.py
+++ SIR_adaptive_net_layers.py
@@ -19,7 +19,7 @@ def infneighsLayer1(ntw, node, ns, ilist, IDtoSecondLayer):
     return [node, len(infneigh), infneigh]
 
 
-def infneighsLayer2(ntw, node, ns, ilist, IDtoSecondLayer):
+def infneighsLayer2(node, ilist, IDtoSecondLayer):
     inf = [k for k in range(len(ilist)) if ilist[k] == 1]
     neighlist = IDtoSecondLayer[node][1]
     infneigh = [neigh for neigh in neighlist if neigh in inf]
@@ -28,27 +28,16 @@ def infneighsLayer2(ntw, node, ns, ilist, IDtoSecondLayer):
 
 def sporadicRun(idx, n, T, v, T2, v2, pi, pr, ns, net, max_layer_2, it):
 
+    bepidemic_ = pd.DataFrame({})
+
     i0 = random.randint(0, n-1)
     i = [1 if k == i0 else 0 for k in range(n)]
     s = [1 - k for k in i]
     r = [0]*n
 
-    if not isinstance(v, list):
-        v = [v]*n
-
-    if not isinstance(T, list):
-        T = [T]*n
-
-    if not isinstance(v2, list):
-        v2 = [v2]*n
-
-    if not isinstance(T2, list):
-        T2 = [T2]*n
-
     rednet = net.copy()
-    # print(f'Edges initial: {rednet.number_of_edges()}')
 
-    net_contact_dict0 = {
+    net_contact_dict = {
         node: len([neig for neig in rednet.neighbors(node)]) for node in range(n)
     }
 
@@ -63,18 +52,22 @@ def sporadicRun(idx, n, T, v, T2, v2, pi, pr, ns, net, max_layer_2, it):
             for new_conn in node[1]:
                 rednet.add_edge(node[0], new_conn)
 
-    net_contact_dict = {
-        node: len([neig for neig in rednet.neighbors(node)]) for node in range(n)
-    }
+    print(sum([len(node[1]) for node in IDtoSecondLayer if s[node[0]] == 1]))
 
-    bepidemic_ = pd.DataFrame({})
-    contacts_df_ = pd.DataFrame({})
+    if not isinstance(v, list):
+        v = [v]*n
+
+    if not isinstance(T, list):
+        T = [T]*n
+
+    if not isinstance(v2, list):
+        v2 = [v2]*n
+
+    if not isinstance(T2, list):
+        T2 = [T2]*n
 
     for t in range(it):
 
-        # print(f'day = {t}')
-        
-        # Use edges variable
         A = nx.adjacency_matrix(rednet).todense()
         infn = np.matmul(A, i)
         infn = infn[0, :].tolist()[0]
@@ -105,22 +98,25 @@ def sporadicRun(idx, n, T, v, T2, v2, pi, pr, ns, net, max_layer_2, it):
         # ===== The maximization process =====
         rednet = net.copy() # Define a dummy network: this is the network to play with
 
-        # Get the info of all susceptible nodes at time t
         sus = [k for k in range(n) if s[k] == 1]
 
-        # List of infected neighbors for each susceptible node
-        # Filter only those that are not in the second layer
-        infofsus = [infneighsLayer1(net, node, 1, inew, IDtoSecondLayer) for node in sus]
+        # Layer 1, as usual
+        
+        # Get the info of all susceptible nodes at time t
+        infofsus = [infneighs(net, node, ns, inew) for node in sus]
 
         # Get only the info of susceptible with  infected neighbours
         susinfs = [k for k in infofsus if k[1]!=0]
 
-        # Layer 1: remove edges from contacts, as long as they are not in Layer 2
-        # Create the list of all the edges to remove at time t: of the form {s, i-rem}
-        redneighslay1 = [vertxmaxer(net, susinfs[k], [pi,pr], [v[susinfs[k][0]], T[susinfs[k][0]]], ns)[0] for k in range(len(susinfs))]
+        # Use the maximizer to define how many edges to drop: it returns the number of edges to leave
+        redneighs = [vertxmaxer(net, susinfs[k], [pi,pr], [v[susinfs[k][0]],T[susinfs[k][0]]], ns)[0]  for k in range(len(susinfs))]
 
-        list_ = [[susinfs[k][0], random.sample(susinfs[k][2], susinfs[k][1] - redneighslay1[k])] for k in range(len(redneighslay1))]
-    
+        # Get the list of the form {susceptible  id,{list of infected neighbours to remove}}
+        list_ = [[susinfs[k][0], random.sample(susinfs[k][2], susinfs[k][1] - redneighs[k])] for k in range(len(redneighs))]
+
+        # Create the list of all the edges to remove at time t: of the form {s,i-rem}
+        # Drop edges from the network
+
         for edges_list in list_:
             flat_edges = [(edges_list[0], edges_list[1][k]) for k in range(len(edges_list[1]))]
             try:
@@ -129,15 +125,21 @@ def sporadicRun(idx, n, T, v, T2, v2, pi, pr, ns, net, max_layer_2, it):
                 print('Error!')
                 pass
 
-        # Layer 2: we add sporadic contacts in this layer.
-        # print(f'Edges before: {rednet.number_of_edges()}')
-        # 2.1 Remove previous second layer connections:
-        for node in IDtoSecondLayer:
-            rednet.remove_edges_from([(node[0], new_conn) for new_conn in node[1]])
-        # print(f'Edges after: {rednet.number_of_edges()}')
-        
+        sus_contacts1 = {}
+
+        for node in sus:
+            if net_contact_dict[node] != 0:
+                sus_contacts1[node] = len([neig for neig in rednet.neighbors(node)])/net_contact_dict[node]
+            else:
+                sus_contacts1[node] = 0
+    
+        sus_contacts_ratios1 = np.nanmean(list(sus_contacts1.values()))
+        number_edges = rednet.number_of_edges() # Without sporadic contacts
+
+        # Sporadic Layer (Layer 2): we add sporadic contacts in this layer.
+
         # 2.2 List of infected layer 2 contacts for each susceptible node
-        infofsus2 = [infneighsLayer2(net, node, 1, inew, IDtoSecondLayer) for node in sus]
+        infofsus2 = [infneighsLayer2(node, inew, IDtoSecondLayer) for node in sus]
 
         # 2.3 Filter only those that are in the second layer
         susinfs2 = [k for k in infofsus2 if k[1]!=0]
@@ -162,7 +164,6 @@ def sporadicRun(idx, n, T, v, T2, v2, pi, pr, ns, net, max_layer_2, it):
 
                     # Sample as many nodes as the vertmaxer said, for this node.
                     j = susnodes2.index(node[0])
-                    # print('For second layer maximizer said', redneighslay2[j], 'contacts')
                     list_2_ = random.sample(pop_to_sample, redneighslay2[j])
                     flat_edges = [(node[0], li) for li in list_2_]
 
@@ -188,52 +189,47 @@ def sporadicRun(idx, n, T, v, T2, v2, pi, pr, ns, net, max_layer_2, it):
                     IDtoSecondLayer[node[0]] = [node[0], []]
                 # Other considerations: recovered nodes can have sporadic contacts. How about sporadic contacts for infected?
 
-        # print(f'Edges final: {rednet.number_of_edges()}')
-        # === Update done, compute day values === #
+        sus_contacts2 = {}
+        for node in sus:
+            sus_contacts2[node] = len(IDtoSecondLayer[node][1])/int(max_layer_2/2)
+
+        sus_contacts_ratios2 = np.nanmean(list(sus_contacts2.values()))
+        number_edges2 = rednet.number_of_edges()
+
+        # print('Number of susceptibles', len(sus))
+        # print('Length of dictionary', len(list(sus_contacts.values())))
+        # print('sus_contacts_ratios', sus_contacts_ratios)
+        # print('Length of dictionary 2', len(list(sus_contacts2.values())))
+        # print('sus_contacts_ratios 2', sus_contacts_ratios2)
 
         sus_contacts = {}
         for node in sus:
-            if net_contact_dict[node] != 0:
-                sus_contacts[node] = len(list(rednet.neighbors(node)))/net_contact_dict[node]
-            else:
-                sus_contacts[node] = 0 
-
+            try:
+                sus_contacts[node] = (sus_contacts1[node]*net_contact_dict[node] + sus_contacts2[node]*(int(max_layer_2/2)))/(net_contact_dict[node]+int(max_layer_2/2))
+            except Exception:
+                pass
         sus_contacts_ratios = np.nanmean(list(sus_contacts.values()))
 
         bepidemic_ = pd.concat([bepidemic_, pd.DataFrame({
             'day': [t+1],
-            's': [sum(snew)],
-            'i': [sum(inew)],
-            'r': [sum(rnew)],
-            'edgecount': [rednet.number_of_edges()],
-            'suscedgecount': sus_contacts_ratios
+            's': [sum(snew)/n],
+            'i': [sum(inew)/n],
+            'r': [sum(rnew)/n],
+            'edgecount': [number_edges2],
+            'suscedgecount': sus_contacts_ratios,
+            'edgecount1': [number_edges],
+            'suscedgecount1': sus_contacts_ratios1,
+            'edgecount2': [number_edges2 - number_edges],
+            'suscedgecount2': sus_contacts_ratios2
         })])
 
-        contacts_df_day = pd.DataFrame({
-            'node': list(range(n)),
-            'L2_contacts': [len(nd[1])/(int(max_layer_2/2)) for nd in IDtoSecondLayer],
-            'L1_contacts': [len([ng for ng in list(rednet.neighbors(node)) 
-                                            if ng not in IDtoSecondLayer[node][1]])/net_contact_dict0[node]
-                                            for node in range(n)]
-        })
-        contacts_df_day['day'] = t
-        contacts_df_ = pd.concat([contacts_df_, contacts_df_day], ignore_index=False)
-
     bepidemic_['edgecount'] = bepidemic_['edgecount']/np.nanmax(bepidemic_['edgecount'])
-    bepidemic_['suscedgecount'] = bepidemic_['suscedgecount']/np.nanmax(bepidemic_['suscedgecount'])
-
-    bepidemic_['s'] = bepidemic_['s']/n
-    bepidemic_['i'] = bepidemic_['i']/n
-    bepidemic_['r'] = bepidemic_['r']/n
-    
-    # bepidemic_.to_csv('./Data/layerexperiment.csv', index=False)
-    # contacts_df_.to_csv('./Data/contactinfo.csv', index=False)
-    # print('End of experiment')
+    bepidemic_['edgecount1'] = bepidemic_['edgecount1']/np.nanmax(bepidemic_['edgecount1'])
+    bepidemic_['edgecount2'] = bepidemic_['edgecount2']/np.nanmax(bepidemic_['edgecount2'])
 
     bepidemic_['index'] = idx
-    contacts_df_['index'] = idx
 
-    return bepidemic_, contacts_df_
+    return bepidemic_
 
 
 def sporadicExperiment():
@@ -243,10 +239,10 @@ def sporadicExperiment():
     n = 500 # Total population
     it = 200 # Days
     
-    T = 14 # Planning horizon Layer 1
+    T = 7 # Planning horizon Layer 1
     v = 0.05 # Risk perception Layer 1
 
-    T2 = 14 # Planning horizon Layer 2
+    T2 = 7 # Planning horizon Layer 2
     v2 = 0.025 # Risk perception Layer 2
 
     pi = 0.02 # Probability of infection
@@ -265,32 +261,38 @@ def sporadicExperiment():
     pool.close()
     pool.join()
 
-    bepidist_ = pd.concat([bep[0] for bep in bepidist0], ignore_index=True)
+    bepidist_ = pd.concat([bep for bep in bepidist0], ignore_index=True)
     bepidist_ = bepidist_.groupby(['day'], as_index=False).agg({
         's': ['mean', 'max', 'min', 'std'],
         'i': ['mean', 'max', 'min', 'std'],
         'r': ['mean', 'max', 'min', 'std'],
+
         'edgecount': ['mean', 'max', 'min', 'std'],
-        'suscedgecount': ['mean', 'max', 'min', 'std'],})
+        'suscedgecount': ['mean', 'max', 'min', 'std'],
+
+        'edgecount1': ['mean', 'max', 'min', 'std'],
+        'suscedgecount1': ['mean', 'max', 'min', 'std'],
+
+        'edgecount2': ['mean', 'max', 'min', 'std'],
+        'suscedgecount2': ['mean', 'max', 'min', 'std'],})
+    
     bepidist_.columns = bepidist_.columns.droplevel()
     bepidist_.columns = ['day', 
+                         
                         's_mean', 's_max', 's_min', 's_std', 
                         'i_mean', 'i_max', 'i_min', 'i_std',
                         'r_mean', 'r_max', 'r_min', 'r_std', 
+                        
                         'edgecount_mean', 'edgecount_max', 'edgecount_min', 'edgecount_std', 
-                        'suscedgecount_mean', 'suscedgecount_max', 'suscedgecount_min', 'suscedgecount_std']
+                        'suscedgecount_mean', 'suscedgecount_max', 'suscedgecount_min', 'suscedgecount_std',
+                        
+                        'edgecount1_mean', 'edgecount1_max', 'edgecount1_min', 'edgecount1_std', 
+                        'suscedgecount1_mean', 'suscedgecount1_max', 'suscedgecount1_min', 'suscedgecount1_std',
+                        
+                        'edgecount2_mean', 'edgecount2_max', 'edgecount2_min', 'edgecount2_std', 
+                        'suscedgecount2_mean', 'suscedgecount2_max', 'suscedgecount2_min', 'suscedgecount2_std']
+    
     bepidist_.to_csv('./Data/sporadicexperiment.csv', index=False)
-
-    contacts_df_ = pd.concat([bep[1] for bep in bepidist0], ignore_index=True)
-    contacts_df_ = contacts_df_.groupby(['day'], as_index=False).agg({
-        'L1_contacts': ['mean', 'max', 'min', 'std'],
-        'L2_contacts': ['mean', 'max', 'min', 'std'],})
-    contacts_df_.columns = contacts_df_.columns.droplevel()
-    contacts_df_.columns = ['day', 
-                    'L1_mean', 'L1_max', 'L1_min', 'L1_std', 
-                    'L2_mean', 'L2_max', 'L2_min', 'L2_std']
-    contacts_df_.to_csv('./Data/sporadicexperimentcontactinfo.csv', index=False)
-
     print("Sporadic contacts experiment done! :)")
 
 
@@ -429,24 +431,17 @@ def twoNetworksRun(idx, n, ns, v, T, v2, T2, pi, pr, it):
 
         bepidemic_ = pd.concat([bepidemic_, pd.DataFrame({
             'day': [t+1],
-            's': [sum(snew)],
-            'i': [sum(inew)],
-            'r': [sum(rnew)],
+            's': [sum(snew)/n],
+            'i': [sum(inew)/n],
+            'r': [sum(rnew)/n],
             'edgecount1': [rednet1.number_of_edges()],
             'suscedgecount1': sus_contacts_ratios1,
             'edgecount2': [rednet2.number_of_edges()],
             'suscedgecount2': sus_contacts_ratios2,
         })])
 
-    bepidemic_['s'] = bepidemic_['s']/n
-    bepidemic_['i'] = bepidemic_['i']/n
-    bepidemic_['r'] = bepidemic_['r']/n
-
     bepidemic_['edgecount1'] = bepidemic_['edgecount1']/np.nanmax(bepidemic_['edgecount1'])
-    bepidemic_['suscedgecount1'] = bepidemic_['suscedgecount1']/np.nanmax(bepidemic_['suscedgecount1'])
-
     bepidemic_['edgecount2'] = bepidemic_['edgecount2']/np.nanmax(bepidemic_['edgecount2'])
-    bepidemic_['suscedgecount2'] = bepidemic_['suscedgecount2']/np.nanmax(bepidemic_['suscedgecount2'])
 
     # bepidemic_.to_csv('./Data/2layerexperiment.csv', index=False)
     # print('End of experiment')
@@ -508,6 +503,26 @@ if __name__ == '__main__':
 
     sporadicExperiment()
     
-    twoNetworksExperiment()
+    # twoNetworksExperiment()
+
+    # n = 500 # Total population
+    # it = 200 # Days
+    
+    # T = 7 # Planning horizon Layer 1
+    # v = 0.05 # Risk perception Layer 1
+
+    # T2 = 7 # Planning horizon Layer 2
+    # v2 = 0.025 # Risk perception Layer 2
+
+    # pi = 0.02 # Probability of infection
+    # pr = 0.03 # Probability of recovery
+    # ns = 1
+    # net = nx.gnp_random_graph(n, 0.025) # Underlying network
+
+    # max_layer_2 = 20
+
+    # idx = 0
+    # bepidemic_ = sporadicRun(idx, n, T, v, T2, v2, pi, pr, ns, net, max_layer_2, it)
+    # bepidemic_.to_csv('./Data/layerexperiment.csv', index=False)
 
     print("End")
